343410fc3c0e135956d75fd78f5d511b8be93736
Jorge P <hi@jorgepardo.dev>
2022-05-14 19:48:44 +0200

    Responsive layout -WIP- + node packages update

--- my-app/src/js/components/header.js
+++ my-app/src/js/components/header.js
@@ -20,14 +20,16 @@ export const Header = (props) => {
 
     return (  
         <div className="Header">
-            <div className="Header_logo"><img className="Logo" src="https://res.cloudinary.com/jorgepardor/image/upload/v1652198954/mainlogo_lmbkm0.png" alt="Logotipo"/></div>
-            <div className="Header_search">
+            <div>
+                <img className="Logo" src="https://res.cloudinary.com/jorgepardor/image/upload/v1652198954/mainlogo_lmbkm0.png" alt="Logotipo"/>
+            </div>
+            <div className='Search'>
                 <form>
-
                     <input type="text" className="Search_form" onChange={inputHandler} placeholder="Are you looking for something?" onKeyPress={keySearch}></input>
-
                 </form>
             </div>
+
+
         </div>
     );
 };
